b5704fbb29d0a540a1287614362999d743762a6a
Frauke <frauke_heedfeld@hotmail.com>
2020-05-28 21:50:34 +0200

    Added bootstrap + navbar

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,15 +4,26 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{ asset('sass/app.scss')}}">
+        <link href = {{ asset("bootstrap/css/bootstrap.css") }} rel="stylesheet" />
+        <link rel="stylesheet" href="/css/app.css">
 
         <title>{{config('app.name')}}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+
     </head>
     <body>
-       @yield('content')
+
+        @include('pages.navbar')
+        <div class="container">
+            @yield('content')
+        </div>
+
+        <script src="./resources/app.js"></script>
 
     </body>
 </html>
---
+++ resources/views/pages/navbar.blade.php
@@ -0,0 +1,12 @@
+NAVBAR
+
+<div class="wrapper">
+    <nav>
+      <div></div>
+      <ul>
+        <li data-dedata="0px" class="active">Home</li>
+        <li data-dedata="160px">Hoe het werkt</li>
+        <li data-dedata="320px">Contact</li>
+      </ul>
+    </nav>
+  </div>
