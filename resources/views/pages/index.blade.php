04c4d3cf205efdd1ea2871d959307516b3afdf40
Astrid Grammen <astrid.grammen@hotmail.com>
2020-06-14 17:49:01 +0200

    cleaned up layoutcode

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,6 +2,23 @@
 
 @section('content')
 
-        <h1>Dit is de home pagina van de psychologen site</h1>
+<section id="hero" class="d-flex align-items-center">
+    
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 d-lg-flex flex-lg-column justify-content-center align-items-stretch pt-5 pt-lg-0 order-2 order-lg-1" data-aos="fade-up">
+              <div>
+                <h1>Vind de juiste therapeut bij jouw in de buurt</h1>
+                <h2>Lorem ipsum dolor sit amet, tota senserit percipitur ius ut, usu et fastidii forensibus voluptatibus. His ei nihil feugait</h2>
+              </div>
+            </div>
+            <div class="col-lg-6 d-lg-flex flex-lg-column align-items-stretch order-1 order-lg-2 hero-img" data-aos="fade-up">
+              <img src="assets/img/hero-img.png" class="img-fluid" alt="">
+            </div>
+          </div>
+        </div>
+    
+</section>
 
+    
 @endsection
