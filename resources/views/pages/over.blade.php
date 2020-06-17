020a538038b8c84c2b1d65c8938723f00e5e79e2
Astrid Grammen <astrid.grammen@hotmail.com>
2020-06-17 17:31:17 +0200

    added styling

--- resources/views/pages/over.blade.php
+++ resources/views/pages/over.blade.php
@@ -6,8 +6,8 @@
     <section id="details" class="details">
         <div class="container">
             <div class="section-title">
-                <h2>App Features</h2>
-                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                <h2>Hoe werkt EASIER?</h2>
+                <p>Welkom op EASIER op deze pagina leggen we uit hoe je snel en gemakkelijk een afspraak kunt maken.</p>
               </div>
   
           <div class="row content">
@@ -15,19 +15,9 @@
               <img src="assets/img/details-1.png" class="img-fluid" alt="">
             </div>
             <div class="col-md-8 pt-4" data-aos="fade-up">
-              <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-              <p class="font-italic">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                magna aliqua.
-              </p>
-              <ul>
-                <li><i class="icofont-check"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                <li><i class="icofont-check"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-                <li><i class="icofont-check"></i> Iure at voluptas aspernatur dignissimos doloribus repudiandae.</li>
-                <li><i class="icofont-check"></i> Est ipsa assumenda id facilis nesciunt placeat sed doloribus praesentium.</li>
-              </ul>
-              <p>
-                Voluptas nisi in quia excepturi nihil voluptas nam et ut. Expedita omnis eum consequatur non. Sed in asperiores aut repellendus. Error quisquam ab maiores. Quibusdam sit in officia
+              <h3>STAP 1</h3>
+              <p >
+                Voor het maken van een afspraak met de door jou gekozen psycholoog kunt u zich aanmelden of registreren. Klik hier om u aan te melden of te registeren. Bij het registreren krijgt u een bevestigingsmail. Zo is uw account bevestigd en kunt u door naar stap 2. 
               </p>
             </div>
           </div>
@@ -37,18 +27,9 @@
               <img src="assets/img/details-2.png" class="img-fluid" alt="">
             </div>
             <div class="col-md-8 pt-5 order-2 order-md-1" data-aos="fade-up">
-              <h3>Corporis temporibus maiores provident</h3>
-              <p class="font-italic">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                magna aliqua.
-              </p>
-              <p>
-                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum
-              </p>
-              <p>
-                Inventore id enim dolor dicta qui et magni molestiae. Mollitia optio officia illum ut cupiditate eos autem. Soluta dolorum repellendus repellat amet autem rerum illum in. Quibusdam occaecati est nisi esse. Saepe aut dignissimos distinctio id enim.
+              <h3>STAP 2</h3>
+              <p >
+                Voor het maken van een afspraak geeft EASIER de mogelijkheid om zelf een psycholoog te kiezen waarmee u een afspraak maakt. We hebben een overzicht van al onze psychologen, hier vertellen ze wie ze zijn en wat ze doen. Klik hier voor het overzicht.
               </p>
             </div>
           </div>
@@ -58,45 +39,14 @@
               <img src="assets/img/details-3.png" class="img-fluid" alt="">
             </div>
             <div class="col-md-8 pt-5" data-aos="fade-up">
-              <h3>Sunt consequatur ad ut est nulla consectetur reiciendis animi voluptas</h3>
-              <p>Cupiditate placeat cupiditate placeat est ipsam culpa. Delectus quia minima quod. Sunt saepe odit aut quia voluptatem hic voluptas dolor doloremque.</p>
-              <ul>
-                <li><i class="icofont-check"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                <li><i class="icofont-check"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-                <li><i class="icofont-check"></i> Facilis ut et voluptatem aperiam. Autem soluta ad fugiat.</li>
-              </ul>
-              <p>
-                Qui consequatur temporibus. Enim et corporis sit sunt harum praesentium suscipit ut voluptatem. Et nihil magni debitis consequatur est.
-              </p>
-              <p>
-                Suscipit enim et. Ut optio esse quidem quam reiciendis esse odit excepturi. Vel dolores rerum soluta explicabo vel fugiat eum non.
+              <h3>STAP 3</h3>
+              <p>Je hebt een psycholoog gekozen die het beste bij u past of die u aanspreekt.
+              Nu kunnen we een afspraak gaan maken. Bij elke psycholoog kunt u de beschikbaarheden bekijken en kiezen welke tijd en datum het beste bij u aansluit. Vervolgens klikt u op “maak een afspraak” en krijgt u een mail ter bevestiging. Zo weet u zeker dat de afspraak is gemaakt. Zorg ervoor dat u bent aangemeld. Na het maken van de afspraak ziet u een overzicht van de gemaakte afspraken, hier kunt u de afspraak nog wijzigen en annuleren. </p>
+              
+              <p class="font-italic">Kijk voor de annuleringsvoorwaarden bij de contact pagina. Voor algemene vragen kunt u terecht bij onze F.A.Q.
               </p>
             </div>
           </div>
-  
-          <div class="row content">
-            <div class="col-md-4 order-1 order-md-2" data-aos="fade-left">
-              <img src="assets/img/details-4.png" class="img-fluid" alt="">
-            </div>
-            <div class="col-md-8 pt-5 order-2 order-md-1" data-aos="fade-up">
-              <h3>Quas et necessitatibus eaque impedit ipsum animi consequatur incidunt in</h3>
-              <p class="font-italic">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                magna aliqua.
-              </p>
-              <p>
-                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum
-              </p>
-              <ul>
-                <li><i class="icofont-check"></i> Et praesentium laboriosam architecto nam .</li>
-                <li><i class="icofont-check"></i> Eius et voluptate. Enim earum tempore aliquid. Nobis et sunt consequatur. Aut repellat in numquam velit quo dignissimos et.</li>
-                <li><i class="icofont-check"></i> Facilis ut et voluptatem aperiam. Autem soluta ad fugiat.</li>
-              </ul>
-            </div>
-          </div>
-  
         </div>
       </section><!-- End Details Section -->
         
